0324489ff3e49140906942404ec56bd74ed31d49
janilso <janilsorodrigues10@gmail.com>
2020-11-03 17:59:11 -0300

    Creates Scream Splash

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -12,31 +12,13 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
-  // AnimationController _animationCtr;
-  // Animation<double> _animation;
-
   @override
   void initState() {
     super.initState();
-
-    // Timer(Duration(seconds: 4), ()=>Mate)
-
-    // _animationCtr = AnimationController(
-    //     duration: const Duration(milliseconds: 10000),
-    //     vsync: this,
-    //     value: 0,
-    //     lowerBound: 0,
-    //     upperBound: 1);
-    // _animation =
-    //     CurvedAnimation(parent: _animationCtr, curve: Curves.fastOutSlowIn);
-    // // _animation =
-    // //     Tween(begin: );
-    // _animationCtr.forward();
   }
 
   @override
   void dispose() {
-    // _animationCtr.dispose();
     super.dispose();
   }
 
@@ -51,11 +33,6 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
       child: Stack(
         alignment: AlignmentDirectional.center,
         children: [
-          // FadeTransition(
-          //   opacity: _animation,
-          //   child: ,
-          // ),
-
           Center(
             child: EffectCurve(
               child: Container(
@@ -64,20 +41,10 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
               ),
             ),
           ),
-
-          // FadeTransition(
-          //   opacity: _animation,
-          //   child: Center(
-          //     child: Container(
-          //       width: 200,
-          //       child: Image.asset(ImagesAssets.logoVW),
-          //     ),
-          //   ),
-          // ),
           Positioned(
               bottom: 100,
               child: CircularProgressIndicator(
-                backgroundColor: primaryColor,
+                backgroundColor: primaryColor.withAlpha(70),
               ))
         ],
       ),
